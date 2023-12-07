56b6da585159156d2aee83dc34a886b908c2bbbb
tmerca <amercadal202233@iesjoanramis.org>
2023-12-07 11:00:39 +0100

    ey

--- script/script.js
+++ script/script.js
@@ -1,69 +1,107 @@
-// ARRAY CON PALABRAS/LETRAS
-let palabras = ['PLAYA','SATURNO','GRIETA','ABISMO','LAMENTO'];
-let palabraAAdivinar = palabras[Math.floor(Math.random() * palabras.length)];
+function juego() {
 
-// CAPTURANDO ELEMENTOS CON EL DOM
-const divBotones = document.getElementById('botones');
-const divLetra = document.querySelectorAll('.letra:not(.incorrecta, .correcta)');
-const divPalabra = document.getElementById('palabra');
-const botonPopup = document.querySelector('.boton-popup');
-const envoltorioPopup = document.querySelector('.envoltorio-popup');
-const cerrarPopup = document.querySelector('.cerrar-popup');
-const letrasCorrectas = document.getElementById('correctas');
-const letrasIncorrectas = document.getElementById('incorrectas');
+    let divPalabra = document.getElementById('palabra'); // Palabra en Pantalla
+    let divLetrasFalladas = document.getElementById('letras-incorrectas');
+    let mensajeFinal = document.getElementById('contenedor-mensaje-final');
+    let botones = document.getElementById('botones'); // Div con los botones de las letras
+    let palabras = ['ARBOL', 'ARMARIO', 'CAMPANA', 'PALA'];
+    let guiones = []; // Palabra aleatoria en "_"
+    let letrasFalladas = []; // Letras que falla durante la partida
+    let palabraAAdivinar = palabras[Math.floor(Math.random() * palabras.length)]; // Palabra aleatoria del array
+    let palabraElegida = palabraAAdivinar;
+    let adivinada = true; // Palabra adivinada o no
+    let vidas = document.getElementById('intentos');
+    vidas.innerHTML = 7;
+    let letraNoEncontrada = true; // Para comprobar si está en letrasFalladas[]
 
+    // POP-UP
+    const botonPopup = document.querySelector('.boton-popup');
+    const envoltorioPopup = document.querySelector('.envoltorio-popup');
+    const cerrarPopup = document.querySelector('.cerrar-popup');
 
-//LLAMADAS A FUNCIONES 
-escribirGuiones();
-compruebaLetra(palabraAAdivinar);
-mostrarPopup();
+    const divLetra = document.querySelectorAll('.letra:not(.incorrecta, .correcta)');
 
-function mostrarPopup() {
-
-    botonPopup.addEventListener('click', () =>{
-        envoltorioPopup.style.display = 'block';
-    });
-
-    cerrarPopup.addEventListener('click', () =>{
-        envoltorioPopup.style.display = 'none';
-    });
-
-    envoltorioPopup.addEventListener('click', () => {
-        envoltorioPopup.style.display = 'none';
-    });
-
-}
-
-
-// FUNCION QUE NOS ESCRIBE TANTOS GUIONES COMO LETRAS TIENE LA PALABRA A ADIVINAR
-function escribirGuiones(){
-    let guiones = "";
-
-    for(i = 1; i <= palabraAAdivinar.length; i++){
-        guiones += "_ ";
-    }
-    divPalabra.textContent = guiones;
-}
-
-// FUNCION QUE COMPRUEBA QUE LA LETRA PULSADA POR EL CLIENTE ESTÉ EN LA PALABRA A ADIVINAR
-function compruebaLetra(palabra) {
-
-    // Evento para que nos capture la letra pulsada y bucle for para ir comparando letra por letra con la palabra
-    divBotones.addEventListener('click', (e) => {
+    crearEspacios();
+    cambiarEspacios();
+    mostrarPopup();
     
-        for(i = 0; i < palabra.length; i++) {
-            // Hacemos que si coincide añada la clase correspondiente
-            if(e.target.value == palabra[i]) {
-                console.log(e.target.value + " en la posición " + (i + 1));
-                e.target.style= "display: none;";
-                letrasCorrectas.innerHTML = letrasCorrectas.innerHTML + " " + e.target.value;
-            } 
-            if(!e.target.value == palabra[i]){
-                e.target.style= "display: none;";
-                letrasIncorrectas.innerHTML = letrasIncorrectas.innerHTML + " " + e.target.value;
+
+    function mostrarPopup() {
+    
+        botonPopup.addEventListener('click', () =>{
+            envoltorioPopup.style.display = 'block';
+        });
+        
+        cerrarPopup.addEventListener('click', () =>{
+            envoltorioPopup.style.display = 'none';
+        });
+        
+        envoltorioPopup.addEventListener('click', () => {
+            envoltorioPopup.style.display = 'none';
+        });
+        
+    }
+
+    function crearEspacios(){
+        for(let i = 0; i < palabraElegida.length; i++){
+            guiones[i] = "__";
+        }
+    }
+    
+    function cambiarEspacios(){
+        for(let i = 0; i < palabraElegida.length; i++){
+            divPalabra.innerHTML = divPalabra.innerHTML  + " " + guiones[i];
+        }
+    }
+    
+    function comprobarLetra(letra){
+        
+        for(let i = 0; i < palabraElegida.length; i++) {
+            
+            if(letra == palabraElegida.charAt(i)){
+                guiones[i] = letra.toUpperCase();
+                letraNoEncontrada = false;
             }
         }
-
-    });
-
+        if(letraNoEncontrada == true){
+            letrasFalladas[letrasFalladas.length] = " " + letra;
+            vidas.innerHTML = vidas.innerHTML - 1;
+        }
+        
+        divPalabra.innerHTML = "";
+        divLetrasFalladas.innerHTML = letrasFalladas;
+        cambiarEspacios();
+        compruebaAcierto();
+        letraNoEncontrada = true;
+        adivinada = true;
+    }
+    
+    function compruebaAcierto() {
+        for(let i = 0; i < palabraElegida.length; i++) {
+            
+            if(guiones[i].toUpperCase() != palabraElegida.charAt(i)){
+                adivinada = false;
+            }
+        }
+        if((adivinada == true) && (vidas.innerHTML > 0)){
+            mensajeFinal.innerHTML = "FELICIDADES, HAS GANADO!"
+            mensajeFinal.style.display = "block";
+            botones.style.display = "none";
+        }
+        if(vidas.innerHTML == 0){
+            botones.style.display = "none";
+            mensajeFinal.innerHTML = "HAS PERDIDO! LA PALABRA ERA: <br>" + palabraAAdivinar;
+            mensajeFinal.style.display = "block";
+        }
+    }
+    
+    botones.addEventListener('click', (e) =>  {
+        if(e.target.value != null){
+            e.target.style.display = "none";
+            comprobarLetra(e.target.value);
+        }
+    }, false)
+    
 }
+
+juego();
